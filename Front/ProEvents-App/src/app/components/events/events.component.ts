e3ec448f2ea027c2b6c3de4e10b866d24bfffa07
João Carlos Ribeiro <joao.ribeiro1@outlook.com.br>
2024-04-11 11:29:38 -0300

    feat: add gridEvents

--- Front/ProEvents-App/src/app/components/events/events.component.ts
+++ Front/ProEvents-App/src/app/components/events/events.component.ts
@@ -1,27 +1,59 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
 
-  public events: any;
- 
+  public events: any = [];
+  public filteredEvents: any = [];
 
-  constructor(private http: HttpClient) {}
+  imageWidth: number = 150;
+  imageMargin: number = 2;
+  showImage: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.filteredEvents = this.filterList ? this.filterEvents(this.filterList) : this.events;
+  }
+
+  filterEvents(filterBy: string): any {
+    console.log("filterEvents")
+    console.log(this.events)
+
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      (event: { theme: string; local: string; }) => event.theme.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      event.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    )
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEvents();
   }
 
-  public getEvents(){
-    this.http.get('https://localhost:5223/Event').subscribe( 
-      response => this.events = response,
-      error => console.log(error)
-    );
+  changeImage() {
+    this.showImage = !this.showImage;
+  }
+
+  public getEvents(): void {
+    this.http.get('https://localhost:5223/Event').subscribe({
+      next: (response) => {
+      this.events = response;
+      this.filteredEvents = this.events;
+    },
+    error: (error) => console.log(error)
+  });
   }
 
 }
